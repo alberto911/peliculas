492a3136931c3f651b9aef60e9e8598ea797e95d
Eduardo Pickering <eduardopickering@Eduardos-MacBook-Air-3.local>
2014-11-05 18:19:28 -0600

    Initial Commit

---
+++ peliculas/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  peliculas
+//
+//  Created by Eduardo Pickering on 11/5/14.
+//  Copyright (c) 2014 Alberto Pickering. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
